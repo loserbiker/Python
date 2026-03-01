5b2561b10d0f0391c2af002278d5ce1054c86e23
Can <cansrk@icloud.com>
2026-03-01 12:51:37 +0300

    güncelle

--- dene.py
+++ dene.py
@@ -1,1 +1,12 @@
-print(input("Oyunu Başlatmak İçin Tıkla!"))
+import copy
+a = [[1]]
+b = a.copy()
+
+#a[0] += [5]
+# İç değişim (in-place)
+
+a[0] = a[0] + [5]
+# Yeni obje üretir (referans değişimi)
+
+print(a)
+
